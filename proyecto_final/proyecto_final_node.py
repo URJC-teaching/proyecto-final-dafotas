7b5420e956951123eda17feca387e2544bf5d2a9
pablogg18 <paablogg18@gmail.com>
2026-04-29 19:41:01 +0200

    client created

--- proyecto_final/proyecto_final_node.py
+++ proyecto_final/proyecto_final_node.py
@@ -5,16 +5,20 @@ import math
 from rclpy.duration import Duration
 import time
 
+from simple_hri_interfaces.srv import Speech
+
 class FinalProjectNode(Node):
     def __init__(self):
         super().__init__('final_project_node')
 
-
+        self.client = self.create_client(Speech, '/tts_service')
 
 def main(args=None):
     
     rclpy.init(args=args)
     node = VFFControllerNode()
     rclpy.spin(node)
+
+    
     node.destroy_node()
     rclpy.shutdown()
